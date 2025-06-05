f70fb1b3546bafa0c96751adc7033e1d27d2b459
wds0809 <wds0809@gmail.com>
2025-06-05 16:53:58 +0000

    Desafio: nível novato-Posicionando Navios no Tabuleiro Wanderson

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,10 +5,94 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    // Declaração do tabuleiro (matriz 10x10)
+    int tabuleiro[10][10];
+
+    // Inicializa todas as posições do tabuleiro com 0 (água)
+    for (int linha = 0; linha < 10; linha++) {
+        for (int coluna = 0; coluna < 10; coluna++) {
+            tabuleiro[linha][coluna] = 0;
+        }
+    }
+
+    // Vetores que representam os navios (não são estritamente necessários, mas incluídos conforme especificado)
+    int navio_horizontal[3] = {3, 3, 3};
+    int navio_vertical[3] = {3, 3, 3};
+
+    // Coordenadas iniciais dos navios (pré-definidas no código)
+    int linha_horizontal = 2;
+    int coluna_horizontal = 4;
+
+    int linha_vertical = 5;
+    int coluna_vertical = 7;
+
+    // Validação de posição do navio horizontal (cabe no tabuleiro)
+   if (coluna_horizontal + 3 <= 10) {
+        // Verifica se não há sobreposição
+        int sobreposicao = 0;
+        for (int i = 0; i < 3; i++) {
+            if (tabuleiro[linha_horizontal][coluna_horizontal + i] != 0) {
+                sobreposicao = 1;
+                break;
+            }
+        }
+
+        // Posiciona o navio horizontal se não houver sobreposição
+        if (!sobreposicao) {
+            for (int i = 0; i < 3; i++) {
+                tabuleiro[linha_horizontal][coluna_horizontal + i] = navio_horizontal[i];
+            }
+        } else {
+            printf("Erro: Sobreposição detectada ao posicionar o navio horizontal.\n");
+        }
+    } else {
+        printf("Erro: O navio horizontal excede os limites do tabuleiro.\n");
+    }
+
+    // Validação de posição do navio vertical (cabe no tabuleiro)
+    if (linha_vertical + 3 <= 10) {
+        // Verifica se não há sobreposição
+        int sobreposicao = 0;
+        for (int i = 0; i < 3; i++) {
+            if (tabuleiro[linha_vertical + i][coluna_vertical] != 0) {
+                sobreposicao = 1;
+                break;
+            }
+        }
+
+        // Posiciona o navio vertical se não houver sobreposição
+        if (!sobreposicao) {
+            for (int i = 0; i < 3; i++) {
+                tabuleiro[linha_vertical + i][coluna_vertical] = navio_vertical[i];
+            }
+        } else {
+            printf("Erro: Sobreposição detectada ao posicionar o navio vertical.\n");
+        }
+    } else {
+        printf("Erro: O navio vertical excede os limites do tabuleiro.\n");
+    }
+
+    // Exibição do tabuleiro no console
+    printf("TABULEIRO BATALHA NAVAL \n");
+
+    // Imprime cabeçalho de letras (colunas)
+    printf("   ");
+    for (char letra = 'A'; letra <= 'J'; letra++) {
+        printf("%c ", letra);
+    }
+    printf("\n");
+
+    for (int j = 0; j < 10; j++) {
+        // Imprime número da linha (1 a 10), alinhado
+        printf("%2d ", j + 1);
+        for (int i = 0; i < 10; i++) {
+            printf("%d ", tabuleiro[j][i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
@@ -35,6 +119,3 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
-
-    return 0;
-}
